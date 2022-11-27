194d89d73bc19a67831cae6356e2ae566216f7e6
¨Galina¨ <galina.jounda@gmail.com>
2022-11-27 17:06:48 +0100

    Get Initial Array + output in console

--- Program.cs
+++ Program.cs
@@ -10,6 +10,17 @@ namespace FinalHomeWorkCS
     {
         static void Main(string[] args)
         {
+            string[] input = GetIntialArray();
+
+            Console.WriteLine($"[{string.Join(", ", input)}]");
+            Console.ReadLine();
+        }
+
+        static string[] GetIntialArray()
+        {
+            string source = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus dignissim nec felis sodales aliquet. Morbi sed ullamcorper nunc. Nulla ut urna aliquet turpis suscipit bibendum. Nulla sit amet posuere ligula, in placerat ex. Quisque dignissim eros nulla, quis maximus quam efficitur nec. Etiam vel turpis in lectus luctus ultrices. Ut sed urna vehicula, mollis purus quis, commodo justo. Donec vitae mattis arcu.";
+            string[] array = source.Split(new string[] { ", ", ". "}, StringSplitOptions.None);
+            return array;
         }
     }
 }
