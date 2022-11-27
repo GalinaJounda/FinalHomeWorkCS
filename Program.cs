5700b73d24e2918dcfcd8cb7fbe8f3d14e53b76a
¨Galina¨ <galina.jounda@gmail.com>
2022-11-27 17:25:10 +0100

    debugging

--- Program.cs
+++ Program.cs
@@ -20,20 +20,33 @@ namespace FinalHomeWorkCS
         static string[] GetIntialArray()
         {
             string source = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus dignissim nec felis sodales aliquet. Morbi sed ullamcorper nunc. Nulla ut urna aliquet turpis suscipit bibendum. Nulla sit amet posuere ligula, in placerat ex. Quisque dignissim eros nulla, quis maximus quam efficitur nec. Etiam vel turpis in lectus luctus ultrices. Ut sed urna vehicula, mollis purus quis, commodo justo. Donec vitae mattis arcu.";
-            string[] array = source.Split(new string[] { ", ", ". "}, StringSplitOptions.None);
+            string[] array = source.Split(new string[] { " ", ", ", ". "}, StringSplitOptions.None);
             return array;
         }
 
         static string[] FilterArray(string[] input, int stringLength)
         {
-            string[] output = new string[input.Length];
+            string[] tempArray = new string[input.Length];
+            int count = 0;
             for (int i = 0; i < input.Length; i++)
             {
-                if(input[i].Length <= 3)
+                if(input[i].Length <= stringLength)
                 {
-                    output[i] = input[i];
+                    count++;
                 }
             }
+
+            string[] output = new string[count];
+            int counter = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i].Length <= stringLength)
+                {
+                    output[counter] = input[i];
+                    counter++;
+                }
+            }
+
             return output;
         }
     }
